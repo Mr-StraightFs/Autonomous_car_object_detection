2a39ff078c312f2a8aeb7ea5ad063b4dea56ae50
Mr-StraightFs <amin.lh.mba@gmail.com>
2021-08-21 13:23:56 +0100

    Max Suppresion Function implementation

--- Autocar_detection.py
+++ Autocar_detection.py
@@ -97,3 +97,38 @@ def iou(box1, box2):
     iou = float(inter_area) / float(union_area)
 
     return iou
+
+
+def yolo_non_max_suppression(scores, boxes, classes, max_boxes=10, iou_threshold=0.5):
+    """
+    Applies Non-max suppression (NMS) to set of boxes
+
+    Arguments:
+    scores -- tensor of shape (None,), output of yolo_filter_boxes()
+    boxes -- tensor of shape (None, 4), output of yolo_filter_boxes() that have been scaled to the image size (see later)
+    classes -- tensor of shape (None,), output of yolo_filter_boxes()
+    max_boxes -- integer, maximum number of predicted boxes you'd like
+    iou_threshold -- real value, "intersection over union" threshold used for NMS filtering
+
+    Returns:
+    scores -- tensor of shape (, None), predicted score for each box
+    boxes -- tensor of shape (4, None), predicted box coordinates
+    classes -- tensor of shape (, None), predicted class for each box
+
+    Note: The "None" dimension of the output tensors has obviously to be less than max_boxes. Note also that this
+    function will transpose the shapes of scores, boxes, classes. This is made for convenience.
+    """
+
+    max_boxes_tensor = tf.Variable(max_boxes, dtype='int32')  # tensor to be used in tf.image.non_max_suppression()
+
+    # Use tf.image.non_max_suppression() to get the list of indices corresponding to boxes you keep
+
+    nms_indices = tf.image.non_max_suppression(boxes, scores, max_output_size=max_boxes, iou_threshold=iou_threshold)
+
+    # Use tf.gather() to select only nms_indices from scores, boxes and classes
+
+    scores = tf.gather(scores, nms_indices)
+    boxes = tf.gather(boxes, nms_indices)
+    classes = tf.gather(classes, nms_indices)
+
+    return scores, boxes, classes
