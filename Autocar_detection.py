9c7c1c265b79263d0cb0590c6e859cc81195dabc
Mr-StraightFs <amin.lh.mba@gmail.com>
2021-08-21 13:16:35 +0100

    YOLO Filter function

--- Autocar_detection.py
+++ Autocar_detection.py
@@ -15,3 +15,46 @@ from tensorflow.keras.models import load_model
 from yad2k.models.keras_yolo import yolo_head
 from yad2k.utils.utils import draw_boxes, get_colors_for_classes, scale_boxes, read_classes, read_anchors, preprocess_image
 
+
+# Define
+def yolo_filter_boxes(boxes, box_confidence, box_class_probs, threshold=.6):
+    """Filters YOLO boxes by thresholding on object and class confidence.
+
+    Arguments:
+        boxes -- tensor of shape (19, 19, 5, 4)
+        box_confidence -- tensor of shape (19, 19, 5, 1)
+        box_class_probs -- tensor of shape (19, 19, 5, 80)
+        threshold -- real value, if [ highest class probability score < threshold],
+                     then get rid of the corresponding box
+
+    Returns:
+        scores -- tensor of shape (None,), containing the class probability score for selected boxes
+        boxes -- tensor of shape (None, 4), containing (b_x, b_y, b_h, b_w) coordinates of selected boxes
+        classes -- tensor of shape (None,), containing the index of the class detected by the selected boxes
+
+    Note: "None" is here because you don't know the exact number of selected boxes, as it depends on the threshold.
+    For example, the actual output size of scores would be (10,) if there are 10 boxes.
+    """
+
+    # Step 1: Compute box scores
+
+    box_scores = box_confidence * box_class_probs
+    print(type(box_scores))
+
+    # Step 2: Find the box_classes using the max box_scores, keep track of the corresponding score
+
+    box_classes = tf.math.argmax(box_scores, axis=-1)
+    box_class_scores = tf.math.reduce_max(box_scores, axis=-1, keepdims=False)
+
+    # Step 3: Create a filtering mask based on "box_class_scores" by using "threshold". The mask should have the
+    # same dimension as box_class_scores, and be True for the boxes you want to keep (with probability >= threshold)
+    filtering_mask = (box_class_scores >= threshold)
+
+    # Step 4: Apply the mask to box_class_scores, boxes and box_classes
+
+    scores = tf.boolean_mask(box_class_scores, filtering_mask)
+    boxes = tf.boolean_mask(boxes, filtering_mask)
+    classes = tf.boolean_mask(box_classes, filtering_mask)
+
+
+    return scores, boxes, classes
